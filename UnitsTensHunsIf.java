9571f41e6ec78bd09bff677b676a0155cd8d0f86
Karthik <jeefkarthik036@gmail.com>
2022-03-22 20:19:13 +0530

    third- UC-2 pgm

---
+++ UnitsTensHunsIf.java
@@ -0,0 +1,49 @@
+package com.bridge.D2_assignments;
+import java.util.Scanner;
+
+public class UnitsTensHunsIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int thousand = 0;
+		int hundreds = 0;
+		int tens = 0;
+		int ones = 0;
+		
+		//Input 4 digit number          
+		
+		System.out.print("Enter 4 digit number: ");
+		int number = sc.nextInt();
+		
+		if (number <= 9999 && number > 999) {
+			
+			thousand =  number/1000;
+			System.out.printf("thousands place digit: " + thousand  );
+		
+			hundreds = (number/100)%10;
+			System.out.printf("\nHundreds place digit: " + hundreds);
+			
+			
+			tens = (number/10)%10;  // compare with your code
+			System.out.printf("\nTens place digit: " + tens);
+			
+			
+			ones = (number%10);   // compare with your code
+			System.out.printf("\nOnes place digit: " + ones); 
+			
+			}else {      
+
+					if (number > 9999)
+						System.out.println("\nError! Number more then  digits.");
+					if (number < 1000)
+						System.out.println("Error! Number less then 4 digits.");
+			}
+		
+	}
+	
+}
+
+
+
+
